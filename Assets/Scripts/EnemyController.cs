62b0aa96571bc9c623464c08638dc625acdcc5e2
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-09-17 16:27:54 -0700

    Commit

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    
+
+    public void Start()
+    {
+
+    }
+    public enum States
+    {
+        Patrolling,
+        Attack
+    }
+
+    public States enemyState;
+
     public AudioClip destroySound;
-	// Use this for initialization
+    // Use this for initialization
     public void DestroyMe(Vector3 position)
     {
-        AudioSource.PlayClipAtPoint(destroySound,position);
+        AudioSource.PlayClipAtPoint(destroySound, position);
     }
+
+    public void StateCheck()
+    {
+        switch (enemyState)
+        {
+
+            case States.Patrolling:
+                break;
+
+            case States.Attack:
+                break;
+
+        }
+    }
+
+
 }
